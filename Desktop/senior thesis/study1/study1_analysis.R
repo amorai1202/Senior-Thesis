ed09f319ac80e7e7596c256b897b2488a6345682
Amor Ai <aa@amors-macbook.wifi.local.cmu.edu>
2024-03-27 16:48:43 -0400

    Created separate R file for data cleaning, exported cleaned datasets into separate csv files, all analyses now stored in 1 file

--- Desktop/senior thesis/study1/study1_analysis.R
+++ Desktop/senior thesis/study1/study1_analysis.R
@@ -1,166 +1,23 @@
-# STUDY 1 (Part 2) 
+# STUDY 1 ANALYSIS
 
 # Install packages --------------------------------------------------------
 
 library(tidyverse)
-library(data.table)
+library(crosstable)
+library(multcompView)
+library(corrplot)
 
 # Load data ---------------------------------------------------------------
 
-prescreen_df <- read_csv("data/cleaned_prescreen.csv")
-all_data <- read_csv("data/data_0326.csv")
-
-# Extract IDs from either column
-all_data <- all_data |> 
-  mutate(ProlificID = coalesce(ProlificID, PROLIFIC_PID))
-
-# Filter data -------------------------------------------------------------
-
-# Filter out incomplete surveys + those that don't want their data included
-filter_data <- all_data |> 
-        filter(Finished == "1" & 
-                 # Include data 
-                 debrief_include_data == "1") 
-
-print(paste("Number of participants BEFORE MC:", nrow(filter_data)))
-
-# Manipulation check
-data <- filter_data |>  
-  filter(!((mc_task != "1" &
-              mc_mathability != "1" &
-              mc_diagnostic != "1")))   
-
-print(paste("Number of participants AFTER MC:", nrow(data)))
-# n = 210
+part1_df <- read_csv("data/cleaned_part1.csv")
+part2_df <- read_csv("data/cleaned_part2.csv")
 
 # Make sure same people are doing both parts
-part1_participants <- prescreen_df$ProlificID
-part2_participants <- data$ProlificID 
-all(part2_participants[-c(1:2)] %in% part1_participants)
+part1_participants <- part1_df$ProlificID
+part2_participants <- part2_df$ProlificID 
+all(part2_participants %in% part1_participants)
 
-# Clean data -----------------------------------------
-
-  # Turn empty strings to na's
-  data <- data |> mutate_all(~na_if(., ""))
-  
-  # Add a column for which condition participant was assigned to
-  data <- data |>
-    mutate(Condition = ifelse(!is.na(Control_common_1), "Control",
-                              ifelse(!is.na(Q1), "Implicit",
-                                     "Explicit")))
-  
-  # Calculate task performance
-  correct_answers <- c(
-    '6_150_R' = 2,
-    '7_150_R' = 2,
-    '8_150_R' = 2,
-    '9_150' = 1,
-    '10_150_R' = 2,
-    '11_150' = 1,
-    '12_150' = 1,
-    '13_150' = 1,
-    '14_150_R' = 2,
-    '15_150' = 1,
-    '16_150_R' = 2,
-    '17_150_R' = 2,
-    '18_150_R' = 2,
-    '19_150' = 1,
-    '20_150_R' = 2
-  )
-  
-  # Count the number of matches for each row
-  data$Score <- rowSums(sapply(names(correct_answers), 
-                               function(col) data[[col]] == correct_answers[col] & 
-                                 !is.na(data[[col]])))
-  
-  # Reorder the "Condition" factor variable
-  order <- c("Control", "Implicit", "Explicit")
-  data$Condition <- factor(data$Condition, levels = order)
-  
-  # Recode factor variables
-  data <- data |>
-    mutate(Comparison = 
-             recode_factor(Comparison,
-                           "1" = "10th percentile",
-                           "2" = "25th percentile",
-                           "3" = "50th percentile",
-                           "4" = "75th percentile",
-                           "5" = "90th percentile")
-    )
-
-  
-  clean_data <- data |> 
-    # Manually fix errors
-    mutate(`post-expectation` = case_when(
-      `post-expectation` == "4-5" ~ "5",
-      `post-expectation` == "At least 10" ~ "10",
-      TRUE ~ `post-expectation`)) |>
-    # Convert columns to numeric
-    mutate_at(vars(`Duration (in seconds)`,
-                   Score, 
-                   `pre-expectation`, 
-                   `post-expectation`,
-                   Q6, Age), as.numeric) |> 
-    # Only grab necessary columns
-    dplyr::select(Condition, Score, Age, `pre-expectation`, `post-expectation`, Comparison,
-           Control_common_1, Control_common_2, Control_common_3, Control_common_4, Control_common_5,
-           Control_true_1, Control_true_2, Control_true_3, Control_true_4, Control_true_5,
-           Q1_1_TEXT, `Q2...35`, Q3_1_TEXT, Q4_1_TEXT, Q5, Q6, 
-           Blatant_common_1, Blatant_common_2, Blatant_common_3, Blatant_common_4, Blatant_common_5, Blatant_common_6, 
-           Blatant_true_1, Blatant_true_2, Blatant_true_3, Blatant_true_4, Blatant_true_5, Blatant_true_6, 
-           `STAI_1...54`, `STAI_2...55`, `STAI_3...56`, `STAI_4...57`, `STAI_5...58`, `STAI_6...59`, 
-           Approach2, Approach1, Avoidance3, Approach3, Avoidance2, Avoidance1,
-           mc_task, mc_mathability, mc_diagnostic, 
-           `STAI_1...170`, `STAI_2...171`, `STAI_3...172`, `STAI_4...173`, `STAI_5...174`, `STAI_6...175`, 
-           task_stressful, Pressure, task_opinion, Math_abilities1, Math_abilities2, 
-           RIT1, RIT2, RIT3, RIT4, RIT5, RIT6, 
-           B1, B2, B3_Reverse, 
-           Centrality1, Centrality2, Centrality3, 
-           `Duration (in seconds)`, Feedback, Purpose, ProlificID) |> 
-    #Rename
-    rename(FamilyLanguage = Q1_1_TEXT,
-           OtherLanguage = `Q2...35`,
-           LanguageatHome = Q3_1_TEXT,
-           LanguageonCampus = Q4_1_TEXT,
-           StudentOrg = Q5,
-           NumGenerations = Q6,
-           Pre_Expectation = `pre-expectation`,
-           Post_Expectation = `post-expectation`)
-  
-  #Average DV's
-  avg_data <- clean_data |>
-    # convert to numeric
-    mutate(across(c(7:16, 21:72), as.numeric)) |>   
-    rowwise() |>  
-    # calculate row means and round to 3 decimal places
-    mutate(Control_common = round(mean(c_across(7:11), na.rm = TRUE), 3),
-           Control_true = round(mean(c_across(12:16), na.rm = TRUE), 3),
-           Blatant_common = round(mean(c_across(23:28), na.rm = TRUE), 3),
-           Blatant_true = round(mean(c_across(29:34), na.rm = TRUE), 3),
-           STAI_pre = round(mean(c_across(35:40), na.rm = TRUE), 3),
-           AGQ_approach = round(mean(c_across(c(41,42,44)), na.rm = TRUE), 3),
-           AGQ_avoidance = round(mean(c_across(c(43,45,46)), na.rm = TRUE), 3),
-           STAI_post = round(mean(c_across(50:55), na.rm = TRUE), 3),
-           RIT = round(mean(c_across(61:66), na.rm = TRUE), 3),
-           Belonging = round(mean(c_across(67:69), na.rm = TRUE), 3),
-           Centrality = round(mean(c_across(70:72), na.rm = TRUE), 3)
-    ) |>
-    dplyr::select(Condition, Score, AGQ_approach, AGQ_avoidance,Pre_Expectation,
-                  Post_Expectation, RIT, Belonging, Centrality, STAI_pre, STAI_post,
-                  ProlificID)
-    
-  
-  # Bind prescreen data 
-  
-  prescreen_df <- prescreen_df |> 
-                    #Selecting necessary columns
-                    dplyr::select(ProlificID, Gender, M_Approach, M_Avoidance,P_Approach, P_Avoidance,
-                                  Reading_ID, Racial_ID, Gender_ID,
-                                  FirstGen, FirstGen_US, `1st_immigrant`,
-                                  `1st_age`, `2nd_immigrant`, HighestDegree, Major, STEMMajor, GPA)
-  
-  full_data <- merge(avg_data, prescreen_df, by = "ProlificID", all = FALSE)
-              
+full_data <- read_csv("data/cleaned_full.csv")
 
 # EDA ---------------------------------------------------------------------
 
@@ -178,7 +35,52 @@ table(full_data$Condition)
     annotate("text", x = mean(full_data$Score), y = 25, 
              label = paste("Mean =", round(mean(full_data$Score), 2)), vjust = 1.5, color = "red")
   
-# Analysis ------------------------------------------------------------------
+# Gender distribution
+  round(prop.table(table(full_data$Gender)),2)
+# Age range
+  summary(full_data$Age)
+  mean(full_data$Age)
+  sd(full_data$Age)
+# Highest Degree  
+table(full_data$HighestDegree)
+# STEM major
+round(prop.table(table(full_data$STEMMajor)),2)
+# First gen & immigrant
+round(prop.table(table(full_data$FirstGen)),2)
+round(prop.table(table(full_data$`1st_immigrant`)),3)
+
+
+# Means/SDs ---------------------------------------------------------------
+
+# Summary stats - PRESCREEN
+crosstable(full_data, 
+           cols = c(Gender, Age, GPA, HighestDegree, STEMMajor, 
+                    FirstGen, `1st_immigrant`, `2nd_immigrant`), 
+           num_digits = 2,
+           showNA =  "no") |> 
+  as_flextable()
+
+crosstable(full_data, 
+           cols = c(M_Approach, M_Avoidance, P_Approach, P_Avoidance, 
+                    Math_ID, Reading_ID,
+                    Racial_ID, Gender_ID), 
+           by = Condition,
+           num_digits = 2,
+           showNA =  "no") |> 
+  as_flextable()
+
+# Summary stats - PART 2
+
+crosstable(full_data, 
+           cols = c(Score, AGQ_approach, AGQ_avoidance,
+                    RIT, Belonging, Centrality), 
+           by = Condition,
+           num_digits = 2,
+           showNA =  "no") |> 
+  as_flextable()
+
+
+# ANOVA ------------------------------------------------------------------
 
 # Primary ANOVA (condition x score)
 summary(aov(Score ~ Condition, data = full_data))
@@ -187,13 +89,60 @@ summary(aov(Score ~ Condition, data = full_data))
     geom_boxplot() +
     theme_minimal()
   
-  ggplot(data = full_data, aes(x = Pre_Expectation, y = Score)) +
-    geom_point(alpha = 0.5) +
-    geom_smooth(method = "lm", se = FALSE, color = "blue") +
-    theme_minimal()+
-    facet_wrap(~ Condition)
+# Secondary ANOVA (condition x DVs)
+summary(aov(AGQ_approach ~ Condition, data = full_data))
+summary(aov(AGQ_avoidance ~ Condition, data = full_data))
   
-# T-tests
+  ggplot(data = full_data, aes(x = Condition, y = AGQ_approach)) +
+    geom_boxplot() +
+    theme_minimal()  
+  
+  ggplot(data = full_data, aes(x = Condition, y = AGQ_avoidance)) +
+    geom_boxplot() +
+    theme_minimal()  
+  
+summary(aov(Pre_Expectation ~ Condition, data = full_data))
+summary(aov(Post_Expectation ~ Condition, data = full_data))
+
+  ggplot(data = full_data, aes(x = Condition, y = Pre_Expectation)) +
+    geom_boxplot() +
+    theme_minimal()  
+  
+summary(aov(STAI_pre ~ Condition, data = full_data))
+summary(aov(STAI_post ~ Condition, data = full_data))
+
+  ggplot(data = full_data, aes(x = Condition, y = STAI_pre)) +
+    geom_boxplot() +
+    theme_minimal()  
+  
+summary(aov(RIT ~ Condition, data = full_data))
+summary(aov(Belonging ~ Condition, data = full_data))
+summary(aov(Centrality ~ Condition, data = full_data))
+  
+  ggplot(data = full_data, aes(x = Condition, y = Centrality)) +
+    geom_boxplot() +
+    theme_minimal()  
+  
+
+# T-tests for part 1/2 comparisons ----------------------------------------
+
+t.test(full_data$P_Approach, full_data$AGQ_approach, paired = TRUE)
+t.test(full_data$P_Avoidance, full_data$AGQ_avoidance, paired = TRUE)
+t.test(full_data$Racial_ID, full_data$Centrality, paired = TRUE)
+
+
+# Correlations ------------------------------------------------------------
+
+correlations_data <- full_data |> 
+  select(Score, Pre_Approach = P_Approach, Pre_Avoidance = P_Avoidance,
+         Pre_Centrality = Racial_ID, 
+         Approach = AGQ_approach, Avoidance = AGQ_avoidance,
+         RIT, Belonging, Centrality)
+rquery.cormat(correlations_data)
+
+# Extra -------------------------------------------------------------------
+  
+  # T-tests
   control_only <- subset(full_data, full_data$Condition == "Control")
   implicit_only <- subset(full_data, full_data$Condition == "Implicit")
   explicit_only <- subset(full_data, full_data$Condition == "Explicit")
@@ -202,6 +151,35 @@ summary(aov(Score ~ Condition, data = full_data))
   
   
 # summary(aov(Score ~ Condition + Condition*Pre_Expectation, data = full_data))
+  
+  ggplot(data = full_data, aes(x = Pre_Expectation, y = Score)) +
+    geom_point(alpha = 0.5) +
+    geom_smooth(method = "lm", se = FALSE, color = "blue") +
+    theme_minimal()+
+    facet_wrap(~ Condition)
+  
+# Are the conditions leading to differences in pre-expectation? 
+  
+  ggplot(data = full_data, aes(x = Condition, y = Pre_Expectation)) +
+    geom_boxplot() +
+    theme_minimal()
+  
+# Pairwise comparisons  
+  expectation_test <- aov(Pre_Expectation ~ Condition, data = full_data)
+  Tukey <- TukeyHSD(x=expectation_test, conf.level=0.95) 
+  plot(Tukey, las=1, asp = 0.5, cex.axis = 0.5) 
+  title(main="Pre-Expectation", 
+        col.main="red", cex.main=0.8, line = 1)
+  
+
+  
+  ggplot(data = full_data, aes(x = Pre_Expectation, y = Score)) +
+    geom_point() +
+    geom_smooth(method = "lm", se = FALSE, color = "blue") +
+    theme_minimal()
+  
+  
+  
   
 ##########
 
---
+++ Desktop/senior thesis/study1/study1_cleaning.R
@@ -0,0 +1,244 @@
+# Clean Part 1 & Part 2 raw datasets to use for analysis
+
+library(tidyverse)
+
+# Clean part 1 --------------------------------------------------------------
+
+raw_prescreen <- read_csv("data/raw_prescreen.csv")
+#View(raw_prescreen)
+#colnames(raw_prescreen)
+
+filter_df <- raw_prescreen |> 
+  dplyr::select(23,5,24,26:30,32:66) |> 
+  slice(-c(1:2))
+  
+
+clean_prescreen <- 
+  filter_df |> 
+  # Recode columns
+  mutate(Gender = 
+           recode_factor(Gender,
+                         "1" = "Woman",
+                         "2" = "Man",
+                         "3" = "Non-binary",
+                         "4" = "Other"),
+         FirstGen = 
+           recode_factor(FirstGen,
+                         "1" = "Yes",
+                         "2" = "No",
+                         #Not sure is coded as no
+                         "3" = "No"),
+         FirstGen_US = 
+           recode_factor(FirstGen_US,
+                         "1" = "Yes",
+                         "2" = "No",
+                         #Not sure is coded as no
+                         "3" = "No"),
+         `1st_immigrant` = 
+           recode_factor(`1st_immigrant`,
+                         "1" = "Yes",
+                         "2" = "No",
+                         #Not sure is coded as no
+                         "3" = "No"),
+         `2nd_immigrant` = 
+           recode_factor(`2nd_immigrant`,
+                         "1" = "Yes",
+                         "2" = "No",
+                         #Not sure is coded as no
+                         "3" = "No"),
+         HighestDegree = 
+           recode_factor(HighestDegree,
+                         "1" = "HS/GED",
+                         "2" = "Associate",
+                         "3" = "Bachelor",
+                         "4" = "Master",
+                         "5" = "Doctorate"),
+         STEMMajor = 
+           recode_factor(STEMMajor,
+                         "1" = "STEM",
+                         "2" = "Not STEM",
+                         "3" = "Not STEM")) |> 
+  # Recode participant error
+  mutate(`1st_immigrant` = replace(`1st_immigrant`, 34, "No"),
+         `1st_age` = replace(`1st_age`, 34, NA)) |> 
+  # Recode scale error for Math/Reading items
+  mutate_at(vars(Reading5, Math5, Reading6, Math6), 
+            ~recode(., 
+                    "23" = "1",
+                    "24" = "2",
+                    "25" = "3",
+                    "26" = "4",
+                    "27" = "5")) |> 
+  # Convert character to numeric
+  mutate_at(c(2,7,13:43), as.numeric) 
+
+# Calculate baseline averages
+prescreen_avg <- clean_prescreen |> 
+  rowwise() |> 
+  mutate(M_Approach = round(mean(c_across(c(14,16,20)), na.rm = TRUE), 3),
+         M_Avoidance = round(mean(c_across(c(18,22,24)), na.rm = TRUE), 3),
+         P_Approach = round(mean(c_across(c(15,17,21)), na.rm = TRUE), 3),
+         P_Avoidance = round(mean(c_across(c(19,23,25)), na.rm = TRUE), 3),
+         Approach = round(mean(c_across(c(14,16,20,15,17,21)), na.rm = TRUE), 3),
+         Avoidance = round(mean(c_across(c(18,22,24,19,23,25)), na.rm = TRUE), 3),
+         Math_ID = round(mean(c_across(c(27,29,31,33,35,37)), na.rm = TRUE), 3),
+         Reading_ID = round(mean(c_across(c(26,28,30,32,34,36)), na.rm = TRUE), 3),
+         Racial_ID = round(mean(c_across(38:40), na.rm = TRUE), 3),
+         Gender_ID = round(mean(c_across(41:43), na.rm = TRUE), 3)
+  )
+
+part1_data <- prescreen_avg |> 
+                dplyr::select(c(1:13,44:53))
+
+#write.csv(part1_data,"data/cleaned_part1.csv", row.names = FALSE)
+
+# Clean part 2 ------------------------------------------------------------
+
+all_data <- read_csv("data/data_0326.csv")
+
+# Extract IDs from either column
+all_data <- all_data |> 
+  mutate(ProlificID = coalesce(ProlificID, PROLIFIC_PID))
+
+# Calculate attrition 
+n_part1 <- nrow(part1_data)
+n_part2 <- length(which(all_data$ProlificID[-c(1,2)] %in% part1_data$ProlificID))
+((n_part1 - n_part2) / n_part1) * 100
+
+# Filter out incomplete surveys + those that don't want their data included
+filter_data <- all_data |> 
+  filter(Finished == "1" & 
+           # Include data 
+           debrief_include_data == "1") 
+
+print(paste("Number of participants BEFORE MC:", nrow(filter_data)))
+
+# Manipulation check
+data <- filter_data |>  
+  filter(!((mc_task != "1" &
+              mc_mathability != "1" &
+              mc_diagnostic != "1")))   
+
+print(paste("Number of participants AFTER MC:", nrow(data)))
+# n = 210
+
+### CLEAN DATA
+
+# Turn empty strings to na's
+data <- data |> mutate_all(~na_if(., ""))
+
+# Add a column for which condition participant was assigned to
+data <- data |>
+  mutate(Condition = ifelse(!is.na(Control_common_1), "Control",
+                            ifelse(!is.na(Q1), "Implicit",
+                                   "Explicit")))
+
+# Calculate task performance
+correct_answers <- c(
+  '6_150_R' = 2,
+  '7_150_R' = 2,
+  '8_150_R' = 2,
+  '9_150' = 1,
+  '10_150_R' = 2,
+  '11_150' = 1,
+  '12_150' = 1,
+  '13_150' = 1,
+  '14_150_R' = 2,
+  '15_150' = 1,
+  '16_150_R' = 2,
+  '17_150_R' = 2,
+  '18_150_R' = 2,
+  '19_150' = 1,
+  '20_150_R' = 2
+)
+
+# Count the number of matches for each row
+data$Score <- rowSums(sapply(names(correct_answers), 
+                             function(col) data[[col]] == correct_answers[col] & 
+                               !is.na(data[[col]])))
+
+# Reorder the "Condition" factor variable
+order <- c("Control", "Implicit", "Explicit")
+data$Condition <- factor(data$Condition, levels = order)
+
+# Recode factor variables
+data <- data |>
+  mutate(Comparison = 
+           recode_factor(Comparison,
+                         "1" = "10th percentile",
+                         "2" = "25th percentile",
+                         "3" = "50th percentile",
+                         "4" = "75th percentile",
+                         "5" = "90th percentile")
+  )
+
+
+clean_data <- data |> 
+  # Manually fix errors
+  mutate(`post-expectation` = case_when(
+    `post-expectation` == "4-5" ~ "5",
+    `post-expectation` == "At least 10" ~ "10",
+    TRUE ~ `post-expectation`)) |>
+  # Convert columns to numeric
+  mutate_at(vars(`Duration (in seconds)`,
+                 Score, 
+                 `pre-expectation`, 
+                 `post-expectation`,
+                 Q6, Age), as.numeric) |> 
+  # Only grab necessary columns
+  dplyr::select(Condition, Score, Age, `pre-expectation`, `post-expectation`, Comparison,
+                Control_common_1, Control_common_2, Control_common_3, Control_common_4, Control_common_5,
+                Control_true_1, Control_true_2, Control_true_3, Control_true_4, Control_true_5,
+                Q1_1_TEXT, `Q2...35`, Q3_1_TEXT, Q4_1_TEXT, Q5, Q6, 
+                Blatant_common_1, Blatant_common_2, Blatant_common_3, Blatant_common_4, Blatant_common_5, Blatant_common_6, 
+                Blatant_true_1, Blatant_true_2, Blatant_true_3, Blatant_true_4, Blatant_true_5, Blatant_true_6, 
+                `STAI_1...54`, `STAI_2...55`, `STAI_3...56`, `STAI_4...57`, `STAI_5...58`, `STAI_6...59`, 
+                Approach2, Approach1, Avoidance3, Approach3, Avoidance2, Avoidance1,
+                mc_task, mc_mathability, mc_diagnostic, 
+                `STAI_1...170`, `STAI_2...171`, `STAI_3...172`, `STAI_4...173`, `STAI_5...174`, `STAI_6...175`, 
+                task_stressful, Pressure, task_opinion, Math_abilities1, Math_abilities2, 
+                RIT1, RIT2, RIT3, RIT4, RIT5, RIT6, 
+                B1, B2, B3_Reverse, 
+                Centrality1, Centrality2, Centrality3, 
+                `Duration (in seconds)`, Feedback, Purpose, ProlificID) |> 
+  #Rename
+  rename(FamilyLanguage = Q1_1_TEXT,
+         OtherLanguage = `Q2...35`,
+         LanguageatHome = Q3_1_TEXT,
+         LanguageonCampus = Q4_1_TEXT,
+         StudentOrg = Q5,
+         NumGenerations = Q6,
+         Pre_Expectation = `pre-expectation`,
+         Post_Expectation = `post-expectation`)
+
+#Average DV's
+avg_data <- clean_data |>
+  # convert to numeric
+  mutate(across(c(7:16, 21:72), as.numeric)) |>   
+  rowwise() |>  
+  # calculate row means and round to 3 decimal places
+  mutate(Control_common = round(mean(c_across(7:11), na.rm = TRUE), 3),
+         Control_true = round(mean(c_across(12:16), na.rm = TRUE), 3),
+         Blatant_common = round(mean(c_across(23:28), na.rm = TRUE), 3),
+         Blatant_true = round(mean(c_across(29:34), na.rm = TRUE), 3),
+         STAI_pre = round(mean(c_across(35:40), na.rm = TRUE), 3),
+         AGQ_approach = round(mean(c_across(c(41,42,44)), na.rm = TRUE), 3),
+         AGQ_avoidance = round(mean(c_across(c(43,45,46)), na.rm = TRUE), 3),
+         STAI_post = round(mean(c_across(50:55), na.rm = TRUE), 3),
+         RIT = round(mean(c_across(61:66), na.rm = TRUE), 3),
+         Belonging = round(mean(c_across(67:69), na.rm = TRUE), 3),
+         Centrality = round(mean(c_across(70:72), na.rm = TRUE), 3)
+  )
+  
+#write.csv(avg_data,"data/cleaned_part2.csv", row.names = FALSE)
+
+# Combine both sessions ---------------------------------------------------
+
+part2_data <- avg_data |> 
+                  dplyr::select(Condition, Score, AGQ_approach, AGQ_avoidance,Pre_Expectation,
+                            Post_Expectation, RIT, Belonging, Centrality, STAI_pre, STAI_post, Age,
+                            ProlificID)
+
+full_data <- merge(part2_data, part1_data, by = "ProlificID", all = FALSE)
+
+#write.csv(full_data,"data/cleaned_full.csv", row.names = FALSE)
